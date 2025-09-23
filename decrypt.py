7b83a14ffba54a50c238d63ee42b4bef4ee2039f
Emi959 <161673913+Emi959@users.noreply.github.com>
2025-09-23 01:42:52 -0500

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,22 +1,19 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
 #imports the clock
 from datetime import datetime
-
 # Get the current date and time
 current_time = datetime.now()
-
 # Extract the hour as an integer
 current_hour = current_time.hour
-
 # defines decrpyt_file used on line 26
 def decrypt_file(input_path, output_path="decrypt.txt"):
     try:
         #opens and reads encrypted file
         with open(input_path, "r", encoding="utf-8") as infile:
             encrypted_data = infile.read()
-
         #defines decrypted as how to decrypt and creates a list
         decrypted = ''.join([chr(int(int(num) / 3 / current_hour)) for num in encrypted_data.split()])
-
         #creates ne file
         with open(output_path, "w", encoding="utf-8") as outfile:
         #decrypts in new file
@@ -32,7 +29,13 @@ def decrypt_file(input_path, output_path="decrypt.txt"):
     # prints decryption failiure message
     except Exception as e:
         print("Decryption failed:", e)
-
+root = tk.Tk()
+root.title("File Encryptor")
+label = tk.Label(root, text="Decrypt a text file"
+label.pack(pady=10)
+button = tk.Button(root, text= "select file to decrypt", command=select_input_file)
+button.pack(pady=10)
+root.mainloop()
 # moves text from 'encrypted.txt' to the newly made 'deccrypted.txt' file
 if __name__ == "__main__":
     decrypt_file("encrypted.txt")
